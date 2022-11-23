d91b7b136040e3fa4d72c378f38cc950e9c745cf
Gourang-pro <117590608+Gourang-pro@users.noreply.github.com>
2022-11-23 18:07:28 +0530

    Add files via upload

---
+++ G addition.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+int n1,n2,addition;
+
+printf("the 1st number:");
+scanf("%d",&n1);
+
+printf("the 2nd number:");
+scanf("%d",&n2);
+
+addition=n1+n2; 
+
+printf("addtion=%d",addition);
+
+return 0;
+
+}
+
+
